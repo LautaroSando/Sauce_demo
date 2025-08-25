c889e6137ab4871d28b2c9668fe5ccd9838f34e2
Lautaro-boot <lautaro.sandoval@dualbootpartners.com>
2025-08-24 22:53:14 -0300

    refactor: usar fixture para LoginPage

---
+++ fixtures/fixtures.ts
@@ -0,0 +1,17 @@
+import { test as base } from '@playwright/test';
+import { LoginPage } from '../pages/loginPage';
+
+interface Fixtures {
+    loginPage: LoginPage;
+}
+
+export const test = base.extend<Fixtures>({
+  loginPage: async ({ page }, use) => {
+    // Acá se crea la instancia SOLO una vez por test
+    const loginPage = new LoginPage(page);
+    await use(loginPage);
+  },
+});
+
+
+export { expect } from  '@playwright/test';
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,36 +1,32 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/loginPage.ts';
+import { test, expect} from '../fixtures/fixtures';
 import User from '../data/users.json';
 import Link from '../data/urlData.json';
 import Messages from '../data/messages.json';
 
-let loginPage: LoginPage;
-
-test.beforeEach(async ({ page }) => {
-  loginPage = new LoginPage(page);
+test.beforeEach(async ({ loginPage }) => {
   await loginPage.navigateToLoginPage();
 });
 
-test('TC-01:Login with valid credentials', async ({ page }) => {
+test('TC-01:Login with valid credentials', async ({ loginPage, page }) => {
   await loginPage.completeFormLogin(User.userValido);
   await expect(page).toHaveURL(Link.url.homepage);
 });
 
-test('TC-02:Login with invalid credentials', async ({ page }) => {
+test('TC-02:Login with invalid credentials', async ({ loginPage,page }) => {
   await loginPage.completeFormLogin(User.userInvalid);
   await expect(page).toHaveURL(Link.url.login);
   await expect (loginPage.errorButton).toBeVisible();
   await expect (loginPage.errorButton).toHaveText(Messages.errorMessages.invalidUser);
 });
 
-test('TC-03:Login with empty credentials', async ({ page }) => {
+test('TC-03:Login with empty credentials', async ({ loginPage ,page }) => {
   await loginPage.completeFormLogin(User.userEmpty);
   await expect(page).toHaveURL(Link.url.login);
   await expect (loginPage.errorButton).toBeVisible();
   await expect (loginPage.errorButton).toHaveText(Messages.errorMessages.nullUserAndPassword);
 });
 
-test('TC-04:Login with empty password', async ({ page }) => {
+test('TC-04:Login with empty password', async ({ loginPage ,page }) => {
   await loginPage.completeFormLogin({ user: User.userValido.user, password: '' });
   await expect(page).toHaveURL(Link.url.login);
   await expect (loginPage.errorButton).toBeVisible();
